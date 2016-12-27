3a1611f42ad2cfa2658022bdfc6fc15ebec8cdf6
AyeletEhrman <ayelet099@gmail.com>
2016-12-27 09:57:20 +0200

    from ayelet

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "TaxiFlow.h"
+
+int main() {
+    TaxiFlow flow = TaxiFlow();
+    // gets the input from the user and runs the taxi center.
+    flow.getInput();
+    return 0;
+}
